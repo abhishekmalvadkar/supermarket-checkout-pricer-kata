2e4e230745e7367096adacf076282d74389c5855
Abhishek Malvadkar <abhi@dev.com>
2025-04-08 12:01:05 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/scp/SupermarketCheckoutPricerApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.scp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketCheckoutPricerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketCheckoutPricerApplication.class, args);
+	}
+
+}
